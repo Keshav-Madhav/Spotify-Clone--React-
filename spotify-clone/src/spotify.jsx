9963031a014871f5d552303b5d389f9ac120c6ee
Keshav <keshav2552003@gmail.com>
2023-07-24 13:04:35 +0530

    Initial Commit

---
+++ spotify-clone/src/spotify.jsx
@@ -0,0 +1,5 @@
+//https://developer.spotify.com/documentation/web-playback-sdk.quick-start/#
+
+export const authEndpoint="https://accounts.cpotify.com/authorize";
+
+const redirectUri="http://localhost:5173/" 
