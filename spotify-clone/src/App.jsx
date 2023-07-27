79d6681ac81ca4171338a55224842f6c5b8bdd68
Keshav <keshav2552003@gmail.com>
2023-07-27 23:09:10 +0530

    Set up data layer for fetch and dispatch

--- spotify-clone/src/App.jsx
+++ spotify-clone/src/App.jsx
@@ -2,28 +2,44 @@ import React,{ useEffect, useState} from 'react';
 import './App.css';
 import Login from './Login';
 import { getTokenFromURL } from './Spotify';
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from './Player';
+import { useDataLayerValue } from './DataLayer';
+
+const spotify= new SpotifyWebApi();
 
 function App() {
-  const [token,setToken] = useState(null);
+  const [token,setToken]= useState(null);
+  const [{user}, dispatch]= useDataLayerValue();
 
   useEffect(()=>{
-    const hash = getTokenFromURL();
+    const hash= getTokenFromURL();
 
-    window.location.hash="";
+    window.location.hash= "";
     const _token=hash.access_token;
 
     if(_token){
       setToken(_token);
-    }
 
-    console.log('token:',token)
-  },[]);
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then(user =>{
+
+        dispatch({
+          type:'SET_USER',
+          user: user,
+        })
+      });
+    }
+  }, []);
+
+console.log("user:", user);
 
   return (
     <>
       <div className="app">
         {
-          token ? (<h1>logged in</h1>):(<Login />)
+          token ? <Player/> : <Login />
         }
       </div>
     </>
---
+++ spotify-clone/src/Player.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Player() {
+    return (
+        <h1>Welcome to Spotify</h1>
+    )
+}
+
+export default Player
